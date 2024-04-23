eff9721ab8f9b10668e42956abc24dde83ba55ee
Lukas Bauhaus <bauhaus@antony-it.de>
2024-04-23 21:07:13 +0200

    kommentare

--- src/core-engine/BpmnInstance.cs
+++ src/core-engine/BpmnInstance.cs
@@ -10,15 +10,21 @@ public class BpmnInstance
     public Guid Id { get; set; } = Guid.NewGuid();
     public required BpmnModel Model { get; set; }
     public required Process Process { get; set; }
+    
+    
     public List<Token> Tokens { get; private set; } = [];
     public List<ProcessActivity> Activities { get; private set; } = [];
     public Dictionary<string, object> PricessVariables { get; set; } = new();
     
+    
+    //TODO: anm. v. Lukas: ich finde, es ist kein schöner stil, wenn eine Methode
+    //einfach nur daten aus eingabedaten weiterleitet. Wofür?
     public List<FlowNode> GetPossibleInteractionFlowNodes(Token token)
     {
         return Model.GetPossibleInteractionFlowNodes(token);
     }
 
+    //TODO: anm. v. Lukas: was genau bedeutet denn "Start"? Was soll denn hier passieren?
     public void Start(FlowNode? flowNode = null)
     {
         // Wenn flowNode null ist, wird der Prozess gestartet mit allen StartEvents etc.
@@ -34,6 +40,8 @@ public class BpmnInstance
         throw new NotImplementedException();
     }
     
+    //TODO: anm. v. Lukas: was ist denn ein "Event"?  Hier wird doch gar nix geworfen? Oder 
+    // ist die Rückmeldung einer Aktivität gemeint?
     public void ThrowEvent(ThrowEvent throwEvent, Dictionary<string, object> variables)
     {
         // Hier wird ein Event geworfen
--- src/core-engine/ProcessActivity.cs
+++ src/core-engine/ProcessActivity.cs
@@ -8,10 +8,21 @@ public class ProcessActivity
     public Guid Id { get; set; } = Guid.NewGuid();
     public required FlowNode ActualNode { get; set; }
     public int RemainingRetries { get; set; } = 3;
+    
+    //TODO: anm. v. Lukas: das ist m.M.n aufgabe der "App",
+    //da es von der implementierung der "außeren" umgebung abhängt,
+    //ob locking notwendig ist oder nicht. das muss also teil des Datenbankmodells werden
     public DateTime? LockUntil { get; set; } // Wenn gesetzt, ist die Aktivity gesperrt, da sie bearbeitet wird
+    
     public ActivityState State { get; set; } = ActivityState.Ready;
     public required Dictionary<string, object> InputVariables { get; set; }
+    
+    //TODO: anm. v. Lukas: output macht hier m.M.n. keinen sinn, da die ja teil
+    // der rückmeldung einer aktivität sind. das mapping ansich sollte der core machen
     public required Dictionary<string, object> OutputVariables { get; set; }
+    
     public required Token CallingToken { get; set; }
+    
+    //TODO: anm. v. Lukas: was genau macht das hier?
     public Dictionary<DateTime, ActivityState> TimeLog { get; init; } = [];
 }
