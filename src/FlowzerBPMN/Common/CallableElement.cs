6f3c5866cd2b120d3edfd7da0ff1e5ab9eb63350
Christian Maaß <christian@maass.it>
2024-05-21 21:25:41 +0200

    Interface nullable warning

--- src/FlowzerBPMN/Common/CallableElement.cs
+++ src/FlowzerBPMN/Common/CallableElement.cs
@@ -9,6 +9,6 @@ public interface ICallableElement : IRootElement
     public string? Name { get; init; }
     
     public InputOutputSpecification? IoSpecification { get; init; }
-    public FlowzerList<InputOutputBinding> IoBindings { get; init; }
-    public FlowzerList<Interface> SupportedInterfaceRefs { get; init; }
+    public FlowzerList<InputOutputBinding>? IoBindings { get; init; }
+    public FlowzerList<Interface>? SupportedInterfaceRefs { get; init; }
 }
