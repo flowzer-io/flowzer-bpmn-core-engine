39328a91d8749495d49436309665bd1db3e4484d
Christian Maaß <christian@maass.it>
2024-05-22 02:57:48 +0200

    Implementierung

--- src/core-engine/ProcessEngine.cs
+++ src/core-engine/ProcessEngine.cs
@@ -1,4 +1,5 @@
 using BPMN.Events;
+using BPMN.Flowzer.Events;
 using BPMN.Process;
 using Model;
 
@@ -6,27 +7,38 @@ namespace core_engine;
 
 public class ProcessEngine(Process process)
 {
-    
+    public Process Process { get; set; } = process;
+
     public InstanceEngine StartProcess(Variables? data = null)
     {
-        var instance = new InstanceEngine(new ProcessInstance {ProcessModel = process});
+        var instance = new InstanceEngine(new ProcessInstance { ProcessModel = Process });
         instance.Start(data);
         return instance;
     }
-    
-    public List<TimerEventDefinition> ActiveTimers()
+
+    public IEnumerable<TimerEventDefinition> ActiveTimers()
     {
-        throw new NotImplementedException();
+        return Process.FlowElements
+            .OfType<FlowzerTimerStartEvent>()
+            .Select(e => e.TimerDefinition);
     }
 
-    public List<MessageDefinition> GetActiveCatchMessages()
+    public IEnumerable<MessageDefinition> GetActiveCatchMessages()
     {
-        throw new NotImplementedException();
+        return Process.FlowElements
+            .OfType<FlowzerMessageStartEvent>()
+            .Select(e => new MessageDefinition { Name = e.Message.Name });
     }
 
-    public List<SignalDefinition> GetActiveCatchSignals()
+    public IEnumerable<SignalDefinition> GetActiveCatchSignals()
     {
-        throw new NotImplementedException();
+        return Process.FlowElements
+            .OfType<FlowzerSignalStartEvent>()
+            .Select(e => new SignalDefinition
+            {
+                Id = e.Signal.FlowzerId ?? "", 
+                Name = e.Signal.Name
+            });
     }
 
     public Task<ProcessInstance> HandleTime(DateTime time)
@@ -34,7 +46,8 @@ public class ProcessEngine(Process process)
         throw new NotImplementedException();
     }
 
-    public Task<ProcessInstance> HandleMessage(string messageName, string? correlationKey = null, object? messageData = null)
+    public Task<ProcessInstance> HandleMessage(string messageName, string? correlationKey = null,
+        object? messageData = null)
     {
         throw new NotImplementedException();
     }
